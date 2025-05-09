32deff0f9dab491d376712cc51fd4c7ef2644207
Jhonjaris Soto <jhons1909@gmail.com>
2025-05-08 22:01:04 -0400

    email for contact me section secured

--- js/config.js
+++ js/config.js
@@ -1,5 +1,13 @@
 const config = {
-    EMAIL_PUBLIC_KEY: 'xNNtCYDB4aaDqG2WR', 
-    EMAIL_SERVICE_ID: 'service_ps0feck',
-    EMAIL_TEMPLATE_ID: 'template_izpsw97',
-}
+    EMAIL_PUBLIC_KEY: process.env.PUBLIC_KEY,
+    EMAIL_SERVICE_ID: process.env.EMAIL_SERVICE_ID,
+    EMAIL_TEMPLATE_ID: process.env.EMAIL_TEMPLATE_ID
+    
+  };
+
+  export default config;
+// const config = {
+//     EMAIL_PUBLIC_KEY: 'xNNtCYDB4aaDqG2WR', 
+//     EMAIL_SERVICE_ID: 'service_ps0feck',
+//     EMAIL_TEMPLATE_ID: 'template_izpsw97',
+// }
