ee1773ed3873ebd178f6e4e1f4cc82e1152f507b
Jhonjaris Soto <jhons1909@gmail.com>
2025-05-10 08:13:20 -0400

    added secure mailing with github secrets

--- js/config.js
+++ js/config.js
@@ -6,8 +6,3 @@ const config = {
   };
 
   export default config;
-// const config = {
-//     EMAIL_PUBLIC_KEY: 'xNNtCYDB4aaDqG2WR', 
-//     EMAIL_SERVICE_ID: 'service_ps0feck',
-//     EMAIL_TEMPLATE_ID: 'template_izpsw97',
-// }
---
+++ js/config.local.js
@@ -0,0 +1,5 @@
+const config = {
+    EMAIL_PUBLIC_KEY: 'xNNtCYDB4aaDqG2WR', 
+    EMAIL_SERVICE_ID: 'service_ps0feck',
+    EMAIL_TEMPLATE_ID: 'template_izpsw97',
+}
